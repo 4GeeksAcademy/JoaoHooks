4cf17c8f78df17562b730c1a38ed1473bc523737
joaojcfr <joao.ferreira95@yahoo.com.br>
2024-05-14 10:15:46 +0000

    Fixed functionality of show and Hide button. system working as expected

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,10 +8,7 @@ const Home = () => {
 
 	const [selected, setSelected] = useState("")
 
-	const handleSelected = (color) => {
-		setSelected(color)
-		setShowBtn(false)
-	}
+	const handleSelected = (color) => {setSelected(color)}
 
 	const [showBtn, setShowBtn] = useState(false)
 
@@ -19,19 +16,19 @@ const Home = () => {
 		<div className="text-center">
 			<div className="post"></div>
 			<div className="box">
-				<div className={`${selected === "circle-red"? "red-selected" : "circle-red"} circle-red`} onClick={()=>handleSelected("circle-red")}>
+				<div className={`${selected === "circle-red"? "red-selected" : "circle-red"}`} onClick={()=>handleSelected("circle-red")}>
 					
 				</div>
-				<div className={`${selected === "circle-yellow"? "yellow-selected" : "circle-yellow"} circle-yellow`} onClick={()=>handleSelected("circle-yellow")}>
+				<div className={`${selected === "circle-yellow"? "yellow-selected" : "circle-yellow"}`} onClick={()=>handleSelected("circle-yellow")}>
 					
 				</div>
-				<div className={`${selected === "circle-green"? "green-selected" : "circle-green"} circle-green`} onClick={()=>handleSelected("circle-green")}>
+				<div className={`${selected === "circle-green"? "green-selected" : "circle-green"}`} onClick={()=>handleSelected("circle-green")}>
 					
 				</div>
-				{showBtn?<div className={`${selected === "circle-purple"? "purple-selected" : "circle-purple"} circle-purple`} onClick={()=>handleSelected("circle-purple")}></div>:""}
+				{showBtn ? <div className={`${selected === "circle-purple"? "purple-selected" : "circle-purple"}`} onClick={()=>handleSelected("circle-purple")}></div>:""}
 					
 			</div>
-			<div className="btn btn-warning" onClick={() => setShowBtn(true)}>click for surpriese</div>
+			<div className="btn btn-warning" onClick={() => setShowBtn(!showBtn)}>click for surprise</div>
 			
 		</div>
 	);
